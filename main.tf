9d225efef8ac7392e3c7c4adb7e14f45774f45b9
Frank Chen <frank.chen@tamr.com>
2022-07-12 13:47:18 -0400

    change back to local file

--- main.tf
+++ main.tf
@@ -85,14 +85,14 @@ data "template_file" "default_tamr_config" {
   }
 }
 
-resource "google_storage_bucket_object" "populated_config_file" {
-  name    = "test_config"
-  content = local.tamr_config
-  bucket  = var.tamr_filesystem_bucket
+resource "local_file" "populated_config_file" {
+  count    = 1
+  filename = "test_config"
+  content  = local.tamr_config
 }
 
-resource "google_storage_bucket_object" "populated_dataproc_file" {
-  name    = "test_dataproc"
-  content = local.dataproc_config
-  bucket  = var.tamr_filesystem_bucket
+resource "local_file" "populated_dataproc_file" {
+  count    = 1
+  filename = "test_dataproc"
+  content  = local.dataproc_config
 }
