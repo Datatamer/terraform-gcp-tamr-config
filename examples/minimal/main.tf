042c2fe56ed0ee27d6191538db86ace6a3b80882
Frank Chen <frank.chen@tamr.com>
2022-07-19 11:54:27 -0400

    chore: address pr suggestions

--- examples/minimal/main.tf
+++ examples/minimal/main.tf
@@ -2,31 +2,31 @@ module "minimal" {
   source = "../../"
 
   # tamr VM
-  tamr_instance_project = var.project_id
-  tamr_instance_name            = var.tamr_instance_name
-  tamr_instance_zone            = var.zone_id
-  tamr_instance_subnet          = var.subnet_uri
-  tamr_instance_service_account = var.service_account.email
-  tamr_instance_internal_ip     = var.tamr_instance_internal_ip
+  tamr_instance_project         = "project_id"
+  tamr_instance_name            = "<tamr_instance_name>"
+  tamr_instance_zone            = "us-east1-b"
+  tamr_instance_subnet          = "<subnet_uri>"
+  tamr_instance_service_account = "<service_account_email>"
+  tamr_instance_internal_ip     = "<tamr_instance_internal_ip>"
   # bigtable config
-  tamr_bigtable_project_id  = var.project_id
-  tamr_bigtable_instance_id = var.bigtable_instance_id
-  tamr_bigtable_cluster_id  = var.bigtable_cluster_id
+  tamr_bigtable_project_id  = "<project_id>"
+  tamr_bigtable_instance_id = "<bigtable_instance_id>"
+  tamr_bigtable_cluster_id  = "<bigtable_cluster_id>"
   tamr_bigtable_min_nodes   = 1
   tamr_bigtable_max_nodes   = 60
   # dataproc
-  tamr_dataproc_project_id = var.project_id
-  tamr_dataproc_bucket     = var.dataproc_bucket_name
-  tamr_dataproc_region     = var.region_id
+  tamr_dataproc_project_id = "<project_id>"
+  tamr_dataproc_bucket     = "<dataproc_bucket_name>"
+  tamr_dataproc_region     = "us-east1"
   # dataproc_cluster_config
-  tamr_dataproc_cluster_subnetwork_uri = var.subnet_uri
-  tamr_dataproc_cluster_zone = var.zone_id
+  tamr_dataproc_cluster_subnetwork_uri = "<subnet_uri>"
+  tamr_dataproc_cluster_zone           = "us-east1-b"
   # cloud sql
-  tamr_cloud_sql_project  = var.project_id
-  tamr_cloud_sql_location = var.region_id
-  tamr_cloud_sql_name     = var.cloudsql_instance_name
-  tamr_sql_password       = var.tamr_password
+  tamr_cloud_sql_project  = "<project_id>"
+  tamr_cloud_sql_location = "us-east1"
+  tamr_cloud_sql_name     = "<cloudsql_instance_name>"
+  tamr_sql_password       = "<tamr_password>"
   # filesystem
-  tamr_filesystem_bucket = var.tamr_bucket_name
+  tamr_filesystem_bucket = "<bucket_name>"
 }
 
